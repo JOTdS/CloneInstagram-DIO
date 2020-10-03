24c89e006ba48041ffde42ae85eee83786911ae7
otavio.timoteo <otavio.timoteo@ewige.com.br>
2020-10-03 11:17:26 -0300

    Projeto Completo DIO

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <div class="instagram-wrapper">
+        <div class="instagram-phone">
+            <img src="IMG/instagram-celular.png" alt="celular" class="celular">
+        </div>
+        <div class="instagram-continue">
+            <div class="group">
+                <img src="IMG/instagram-logo.png" alt="instagram-logo" class="instagram-logo">
+                <div class="profile-photo">
+                    <img src="IMG/perfil-instagram.png" alt="perfil-instagram" class="perfil-instagram">
+                </div>
+                <a href="#" class="instagram-login">Continue como Boneco</a>
+                <a href="#" class="instagram-logout">Remover conta</a>
+            </div>
+            <div class="group">
+                <p class="not-account">Não é Boneco</p>
+                <p class="notaccount">
+                    <span class="link-blue">Alterar contas</span>
+                    ou
+                    <span class="link-blue">Inscreva-se</span>
+                </p>
+            </div>
+            <div class="get-the-app">
+                <p class="get-app">Baixe o aplicativo</p>
+                <div class="download">
+                    <a href="#" class="app-download"><img src="IMG/apple-button.png"class="imgButton"></a>
+                    <a href="#" class="app-download"><img src="IMG/googlePlay-button.png"class="imgButton"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
